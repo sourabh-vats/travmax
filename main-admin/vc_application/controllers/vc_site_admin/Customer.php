2002c00817fd00f67c83f1fe5648b59568301384
Sourabh Vats <sourabhvats@Sourabhs-MacBook-Air.local>
2023-02-25 00:51:25 +0530

    fix

--- main-admin/vc_application/controllers/vc_site_admin/Customer.php
+++ main-admin/vc_application/controllers/vc_site_admin/Customer.php
@@ -1233,9 +1233,6 @@ Team Divinoindia");
         $data['page_title'] = 'Update User';
 
         if ($this->input->server('REQUEST_METHOD') === 'POST' && $this->input->post('find_customer') != '') {
-            echo 'reached here';
-            var_dump($this->input->post());
-            die();
             $this->form_validation->set_rules('assign_to', 'assign to', 'required|trim');
             $find_user = $this->input->post('assign_to');
             $find_user = trim($find_user);
