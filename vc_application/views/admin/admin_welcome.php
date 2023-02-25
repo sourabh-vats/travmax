3b7a37800683176599474fb259dc9427a1f0da64
Sourabh Vats <sourabhvats@Sourabhs-MacBook-Air.local>
2023-02-25 18:48:14 +0530

    fix

--- vc_application/views/admin/admin_welcome.php
+++ vc_application/views/admin/admin_welcome.php
@@ -370,6 +370,20 @@
   }
   echo validation_errors();
   ?>
+
+<?php
+//Notification if user hasn't selected or booked any package
+if ($has_package) {
+  # code...
+} else {
+  echo '<div class="alert alert-danger">';
+  echo '<a class="close" data-dismiss="alert">×</a>';
+  echo 'You have not booked any package. Please book a package to activate your account. Click Here to book your package.';
+  echo '</div>';
+}
+
+
+?>
 </div>
 <div class="col-sm-12 right-bar">
 
