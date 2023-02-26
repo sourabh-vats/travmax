e6c23e048b667b96c4e743d65885f05ed8e0875f
sourabhvatscoder <sourabhvats96@gmail.com>
2023-02-26 23:26:02 +0530

    fix

--- vc_application/views/admin/select_package.php
+++ vc_application/views/admin/select_package.php
@@ -1,5 +1,5 @@
 <h1>This is select package</h1>
-<div class="row">
+<div class="row d-flex align-items-center justify-content-center flex-wrap">
     <div class="col-md-4">
         <img src="" alt="" height="300px" width="250px">
     </div>
