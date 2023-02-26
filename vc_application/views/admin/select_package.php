1cab26d81892898e67046f39d31748ca37933eba
sourabhvatscoder <sourabhvats96@gmail.com>
2023-02-26 20:04:41 +0530

    fix

--- vc_application/views/admin/select_package.php
+++ vc_application/views/admin/select_package.php
@@ -1,1 +1,21 @@
 <h1>This is select package</h1>
+<div class="row">
+    <div class="col-md-4">
+        <img src="" alt="" height="300px" width="250px">
+    </div>
+    <div class="col-md-4">
+        <img src="" alt="" height="300px" width="250px">
+    </div>
+    <div class="col-md-4">
+        <img src="" alt="" height="300px" width="250px">
+    </div>
+    <div class="col-md-4">
+        <img src="" alt="" height="300px" width="250px">
+    </div>
+    <div class="col-md-4">
+        <img src="" alt="" height="300px" width="250px">
+    </div>
+    <div class="col-md-4">
+        <img src="" alt="" height="300px" width="250px">
+    </div>
+</div>
