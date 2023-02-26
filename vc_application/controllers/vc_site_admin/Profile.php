3b7913393f4d5d2f3a52188fae353c15353ced84
sourabhvatscoder <sourabhvats96@gmail.com>
2023-02-26 17:53:36 +0530

    fix

--- vc_application/controllers/vc_site_admin/Profile.php
+++ vc_application/controllers/vc_site_admin/Profile.php
@@ -44,8 +44,14 @@ class Profile extends CI_Controller
 
         $data['has_package'] = false;
         $data['package_information'] = $this->Users_model->get_package($id);
-        print_r($data['package_information']);
-        die();
+        if(empty($data['package_information'])){
+            echo 'No package information found.';
+            die();
+        }else{
+            print_r($data['package_information']);
+            die();
+        }
+        
 
         $left_count = array_column($team, 'macro');
         $team_consume = array_column($team, 'consume');
