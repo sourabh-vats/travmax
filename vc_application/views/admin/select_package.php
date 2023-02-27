fc52b89abec03e757b8ddaf9916c3d16340f271a
sourabhvatscoder <sourabhvats96@gmail.com>
2023-02-27 23:15:19 +0530

    fix

--- vc_application/views/admin/select_package.php
+++ vc_application/views/admin/select_package.php
@@ -1,8 +1,8 @@
 <h1>This is select package</h1>
-<div class="row d-flex align-items-center justify-content-center flex-wrap">
+<div class="row d-flex align-items-center justify-content-center flex-wrap gap-3">
     <?php foreach ($all_packages as $package) { ?>
         <div class="col-md-4 d-flex justify-content-center">
-            <img src="/assets/images/<?php echo $package['name'] ?>.jpg" alt="">
+            <img class="img-fluid" src="/assets/images/<?php echo $package['name'] ?>.jpg" alt="">
         </div>
     <?php } ?>
 </div>
