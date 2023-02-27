4539383e68be13a4a12b10ad5f1c22a5f665e094
sourabhvatscoder <sourabhvats96@gmail.com>
2023-02-27 23:12:06 +0530

    fix

--- vc_application/views/admin/select_package.php
+++ vc_application/views/admin/select_package.php
@@ -2,7 +2,7 @@
 <div class="row d-flex align-items-center justify-content-center flex-wrap">
     <?php foreach ($all_packages as $package) { ?>
         <div class="col-md-4 d-flex justify-content-center">
-            <img src="" alt="" height="300px" width="250px">
+            <img src="/assets/images/<?php echo $package['name'] ?>.jpg" alt="">
         </div>
     <?php } ?>
 </div>
