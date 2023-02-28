688df286710efe5366c6300cf0e80b2f53bc34d1
sourabhvatscoder <sourabhvats96@gmail.com>
2023-02-28 23:52:58 +0530

    fix

--- assets/js/select_package.js
+++ assets/js/select_package.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     $(".select_package_id").click(function () {
         console.log($(this).attr("title"));
+        console.log($(this).parent().child(".package_information").val());
     });
 });
