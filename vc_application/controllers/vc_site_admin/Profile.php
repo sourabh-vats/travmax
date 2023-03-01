f64bdacb17f43f94178532a37829f79734207d11
sourabhvatscoder <sourabhvats96@gmail.com>
2023-03-02 01:24:27 +0530

    fix

--- vc_application/controllers/vc_site_admin/Profile.php
+++ vc_application/controllers/vc_site_admin/Profile.php
@@ -41,6 +41,8 @@ class Profile extends CI_Controller
         if ($data['has_package']) {
             $data["package_data"] = $this->Users_model->get_package_data($data['package_information'][0]['package_id']);
         }
+        var_dump($data["package_data"]);
+        die();
 
         //Calculate Total Team Members
         $team = array();
