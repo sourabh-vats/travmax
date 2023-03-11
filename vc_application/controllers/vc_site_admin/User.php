795d3b83d28ae8f7287198d93f05e6868e409cdc
sourabhvatscoder <sourabhvats96@gmail.com>
2023-03-11 23:15:48 +0530

    dev

--- vc_application/controllers/vc_site_admin/User.php
+++ vc_application/controllers/vc_site_admin/User.php
@@ -119,7 +119,8 @@ class User extends CI_Controller
 			$this->Users_model->update_profile($is_valid['bliss_id'], array('last_visit' => date('Y-m-d H:i:s')));
 
 			echo '<div class="alert alert-success"></div>';
-			header("LOCATION: " . base_url() . "admin");
+			$data['main_content'] = 'admin/admin_welcome';
+        	$this->load->view('includes/admin/template', $data);
 		} else // incorrect username or password
 		{
 			echo '<div class="alert alert-danger">Username or password is wrong.</div>';
