b318880defe698aaf31191231eec52885cc48438
sourabhvatscoder <sourabhvats96@gmail.com>
2023-03-12 18:12:02 +0530

    dev

--- vc_application/views/includes/admin/header.php
+++ vc_application/views/includes/admin/header.php
@@ -33,7 +33,7 @@
     <div class="container-fluid">
         <div class="row">
             <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block sidebar collapse">
-                <div class="position-sticky pt-3 sidebar-sticky d-flex align-items-center">
+                <div class="position-sticky pt-3 sidebar-sticky d-flex align-items-center justify-content-center">
                     <ul class="nav flex-column">
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="#">
