7b673b574c98fbac003e8cc5216e536a007947b0
sourabhvatscoder <sourabhvats96@gmail.com>
2023-03-12 18:25:22 +0530

    dev

--- vc_application/views/includes/admin/template.php
+++ vc_application/views/includes/admin/template.php
@@ -1,5 +1,5 @@
 <?php $this->load->view('includes/admin/header'); ?>
-<main class="col-md-9 ms-sm-auto col-lg-10 px-md-5">
+<main id="main" class="col-md-9 ms-sm-auto col-lg-10 px-md-5">
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Dashboard</h1>
         <div class="btn-toolbar mb-2 mb-md-0">
