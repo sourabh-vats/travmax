29ca3925e5a58b689b221b2b5a8807cbd4c0e4ed
sourabhvatscoder <sourabhvats96@gmail.com>
2023-03-14 17:26:58 +0530

    dev

--- vc_application/views/admin/home.php
+++ vc_application/views/admin/home.php
@@ -2,7 +2,7 @@
 
 </div>
 <div class="row gap-4">
-    <div class="col-md-5 border grey_bg">
+    <div class="col-md-5 grey_bg p-5">
         <h4>My Package</h4>
         <ul>
             <li>Name: Thailand Beach Prime</li>
@@ -14,7 +14,16 @@
             <li>Installments: 2/11</li>
         </ul>
     </div>
-    <div class="col-md border grey_bg">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam molestiae iure culpa? Minus ullam vitae nihil, suscipit enim eligendi voluptates ipsam et quidem? Voluptatum voluptates veniam dolores dolorem quia delectus!
+    <div class="col-md grey_bg p-5">
+    <h4>My Income</h4>
+        <ul>
+            <li>Name: Thailand Beach Prime</li>
+            <li>Price: Rs. 79900</li>
+            <li>Offered Price: Rs. 69900</li>
+            <li>Payment Plan: EasyNow</li>
+            <li>Amount Paid: Rs. 6600</li>
+            <li>Amount Remaining: Rs. 59000</li>
+            <li>Installments: 2/11</li>
+        </ul>
     </div>
 </div>
