cb4321d86c9401664e9458ef8c41f8d5ee58209b
sourabhvatscoder <sourabhvats96@gmail.com>
2023-03-14 11:54:43 +0530

    dev

--- vc_application/views/admin/package_selected_successfully.php
+++ vc_application/views/admin/package_selected_successfully.php
@@ -1,11 +1,21 @@
 <div class="row d-flex align-items-center justify-content-center flex-wrap">
     <h1 class="text-center">Congratulations</h1>
     <h2 class="text-center">You have selected for Rs. and have taken the plan. Please make a payment of Rs. and share the proof with us</h2>
-    <div class="col-md-6 p-5 border-end">
-        <h2 class="text-center">QR Code</h2>
+    <div class="col-md-6 p-5 border-end text-center">
+        <h2 class="">QR Code</h2>
         <img src="" alt="" height="300px" width="300px" style="background-color: grey;">
     </div>
-    <div class="col-md-6 p-5">
-        <h2 class="text-center">Bank Information</h2>
+    <div class="col-md-6 p-5 text-center">
+        <h2 class="">Bank Information</h2>
+        <h3>Travmax Holidays (Current Account)</h3>
+        <h3>Account No: 2221238344613985</h3>
+        <h3>Bank Name: AU Small Finance Bank</h3>
+        <h3>IFSC: AUBL0002383</h3>
+        <h3>Branch: Sector 8, Chandigarh</h3>
+    </div>
+    <div class="d-flex">
+        <a class="btn btn-primary" href="">Dashboard</a>
+        <a class="btn btn-primary" href="">Upload Proof</a>
+        <a class="btn btn-primary" href="">WhatsApp</a>
     </div>
 </div>
