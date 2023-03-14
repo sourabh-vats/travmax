8e8b24821befe12f28aafdfa689314b38d9f8ec2
sourabhvatscoder <sourabhvats96@gmail.com>
2023-03-14 18:06:04 +0530

    dev

--- vc_application/views/admin/home.php
+++ vc_application/views/admin/home.php
@@ -1,20 +1,20 @@
 <div class="row gap-5">
     <div class="col-md-5 grey_bg p-4">
     </div>
-    <div class="col-md grey_bg p-4">
-        <div class="row">
-            <div class="col">
+    <div class="col-md">
+        <div class="row gap-4 mb-4">
+            <div class="col grey_bg">
                 <h1>Hi</h1>
             </div>
-            <div class="col">
+            <div class="col grey_bg">
                 <h1>Hi</h1>
             </div>
         </div>
-        <div class="row">
-            <div class="col">
+        <div class="row gap-4">
+            <div class="col grey_bg">
                 <h1>Hi</h1>
             </div>
-            <div class="col">
+            <div class="col grey_bg">
                 <h1>Hi</h1>
             </div>
         </div>
