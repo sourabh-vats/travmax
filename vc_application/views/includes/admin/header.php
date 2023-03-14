a829dad37b10517324b0e11702af44dfea597b90
sourabhvatscoder <sourabhvats96@gmail.com>
2023-03-14 12:40:23 +0530

    dev

--- vc_application/views/includes/admin/header.php
+++ vc_application/views/includes/admin/header.php
@@ -23,7 +23,10 @@
         <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="navbar-nav">
+        <div class="navbar-nav w-100 d-flex align-content-space-betweenf wrap">
+            <div class="nav-item text-nowrap">
+                <a class="nav-link px-3" href="#">Date: <?php echo date("l, d M"); ?></a>
+            </div>
             <div class="nav-item text-nowrap">
                 <a class="nav-link px-3" href="#">Sign out</a>
             </div>
