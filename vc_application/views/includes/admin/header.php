b6ae96c924723596d09bc3e308f6fc659b4fd648
sourabhvatscoder <sourabhvats96@gmail.com>
2023-03-14 12:46:30 +0530

    dev

--- vc_application/views/includes/admin/header.php
+++ vc_application/views/includes/admin/header.php
@@ -18,12 +18,12 @@
 </head>
 
 <body>
-    <header id="header_navbar" class="navbar navbar-light sticky-top flex-md-nowrap p-0">
+    <header id="header_navbar" class="navbar navbar-light sticky-top flex-md-nowrap p-0 px-md-5">
         <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#"><img class="img-fluid" src="/assets/front/images/logo_white.png"></a>
         <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="navbar-nav w-100 d-flex flex-row justify-content-space-between wrap">
+        <div class="navbar-nav w-100 d-flex flex-row justify-content-between wrap">
             <div class="nav-item text-nowrap">
                 <p class="nav-link px-3"><i class="bi-calendar-fill"></i> Date: <?php echo date("l, d M"); ?></p>
             </div>
