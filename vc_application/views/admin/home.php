82c9af208d13c91170b36b22f4db1c54d37f9def
sourabhvatscoder <sourabhvats96@gmail.com>
2023-03-14 16:49:44 +0530

    dev

--- vc_application/views/admin/home.php
+++ vc_application/views/admin/home.php
@@ -1,11 +1,11 @@
 <div class="row">
-    
+
 </div>
-<div class="row">
-<div class="col-md-5 border grey_bg">
+<div class="row gap-4">
+    <div class="col-md-5 border grey_bg">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet earum molestiae quasi soluta, at ipsa temporibus nobis quas sit similique est neque velit numquam pariatur recusandae deleniti quae nemo ut?
     </div>
-    <div class="col-md-7 border grey_bg">
+    <div class="col-md border grey_bg">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam molestiae iure culpa? Minus ullam vitae nihil, suscipit enim eligendi voluptates ipsam et quidem? Voluptatum voluptates veniam dolores dolorem quia delectus!
     </div>
 </div>
