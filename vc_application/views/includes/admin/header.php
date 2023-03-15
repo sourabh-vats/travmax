44557fdadb2ac3c80aaafc4255a307b74d3cff00
sourabhvatscoder <sourabhvats96@gmail.com>
2023-03-15 12:27:07 +0530

    dev

--- vc_application/views/includes/admin/header.php
+++ vc_application/views/includes/admin/header.php
@@ -36,8 +36,8 @@
     <div class="container-fluid">
         <div class="row">
             <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block sidebar collapse">
-                <div>
-                    <img src="/images/avatar.png">
+                <div class="pt-3 text-center">
+                    <img class="img-fluid" src="/images/avatar.png">
                     <span><?php echo $full_name; ?></span>
                 </div>
                 <div class="position-sticky pt-3 sidebar-sticky d-flex align-items-center justify-content-center">
