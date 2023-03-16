1321c6f5128b56a9040e82785a5754eb88c38a0a
sourabhvatscoder <sourabhvats96@gmail.com>
2023-03-16 12:18:16 +0530

    dev

--- vc_application/views/admin/package_selected_successfully.php
+++ vc_application/views/admin/package_selected_successfully.php
@@ -1,6 +1,6 @@
 <div class="row d-flex justify-content-center flex-wrap">
     <h1 class="text-center">Congratulations</h1>
-    <h2 class="text-center">You have selected <?php echo $package_data[0]['name']; ?> for Rs. <?php echo $package_data[0]['total']; ?> and have taken the <?php echo $package_information[0]['payment_type']; ?> plan. Please make a payment of Rs. <?php echo $payment_amount; ?>and share the proof with us</h2>
+    <h2 class="text-center">You have selected <?php echo $package_data[0]['name']; ?> for Rs. <?php echo $package_data[0]['total']; ?> and have taken the <?php echo $package_information[0]['payment_type']; ?> plan. Please make a payment of Rs. <?php echo $payment_amount; ?> and share the proof with us</h2>
     <div class="col-md-6 p-5 border-end text-center">
         <h2 class="">QR Code</h2>
         <img src="" alt="" height="250px" width="250px" style="background-color: grey;">
@@ -17,6 +17,6 @@
     <div class="d-flex justify-content-center gap-2 my-3">
         <a class="btn btn-primary" href="/admin">Dashboard</a>
         <a class="btn btn-primary" href="/admin/upload_proof">Upload Proof</a>
-        <a class="btn btn-primary" href="https://wa.me/9216003333">WhatsApp</a>
+        <a class="btn btn-success" href="https://wa.me/9216003333"><i class="bi-whatsapp me-2"></i>WhatsApp</a>
     </div>
 </div>
