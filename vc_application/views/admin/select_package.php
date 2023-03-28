d0f9b96e75f4dcc7ea18a989b4add4fbfc889156
sourabhvatscoder <sourabhvats96@gmail.com>
2023-03-28 23:37:43 +0530

    update

--- vc_application/views/admin/select_package.php
+++ vc_application/views/admin/select_package.php
@@ -3,18 +3,18 @@ $user = $profile[0];
 $full_name = $user['f_name'] . " " . $user['l_name'];
 ?>
 <div class="modal fade" id="start_journey_popup" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-dialog modal-dialog-centered modal-lg">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body text-center">
+            <div class="modal-body text-center p-0">
                 <!-- <h2 class="modal-title" id="exampleModalLabel">Hi <?php echo $full_name; ?></h2> -->
                 <!-- <h3>Start your travel journey now.</h3>
                 <h4>Please select a tour package and a payment plan to continue.</h4> -->
                 <img src="/assets/images/select_package_popup.jpg" class="img-fluid" alt="">
             </div>
-            <div class="modal-footer">
+            <div class="modal-footer text-center" style="background-color: #54578e;">
                 <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Continue</button>
             </div>
         </div>
