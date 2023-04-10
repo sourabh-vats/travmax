6e71027144d1c789f58868141b452437bf305ffd
sourabhvatscoder <sourabhvats96@gmail.com>
2023-04-11 01:08:33 +0530

    dev

--- vc_application/views/admin/package.php
+++ vc_application/views/admin/package.php
@@ -3,3 +3,11 @@
         MALDIVES
     </span>
 </div>
+<div class="row">
+    <div class="col-md-6" id="package_img"></div>
+    <div class="col-md-6">
+        <span class="heading_1">Tour Details</span>
+        <span class="heading_2">4 Nights 5 Days</span>
+
+    </div>
+</div>
