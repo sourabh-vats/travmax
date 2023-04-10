6a5e06c84863a9650baeaf3096a665f1754f2bc1
sourabhvatscoder <sourabhvats96@gmail.com>
2023-04-11 01:19:06 +0530

    dev

--- vc_application/views/admin/package.php
+++ vc_application/views/admin/package.php
@@ -1,4 +1,4 @@
-<div id="banner">
+<div class="row" id="banner">
     <span class="page_heading">
         MALDIVES
     </span>
@@ -7,7 +7,35 @@
     <div class="col-md-6" id="package_img"></div>
     <div class="col-md-6">
         <span class="heading_1">Tour Details</span>
+        <br>
         <span class="heading_2">4 Nights 5 Days</span>
-
+        <br>
+        <span class="heading_2">Original Price: Rs.990000</span>
+        <br>
+        <span class="heading_2">Offered Price: Rs.790000</span>
+        <br>
+        <span class="heading_2">Inclusions</span>
+        <ul>
+            <li>Ex Delhi/Chandigarh Flights</li>
+            <li>Hotels</li>
+            <li>Transfers</li>
+            <li>Breakfast</li>
+            <li>Sightseeing</li>
+        </ul>
+    </div>
+    <div class="col-12">
+        <span class="heading_2">Terms And Conditions</span>
+        <ol>
+            <li>All prices for all tours are on Twin sharing basis. In case of a single traveller, single supplement charges will apply.</li>
+            <li>The tour price includes all the inclusions mentioned and excludes all that’s not mentioned in the inclusions.</li>
+            <li>In the Traveasy plan the tour can be availed after making all the instalments or after making the complete payment at any stage.</li>
+            <li>If a tour is achieved with Travcash as incentive, it cannot be exchanged with cash.</li>
+        </ol>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="terms_check">
+            <label class="form-check-label" for="terms_check">
+                I agree to all terms and conditions.
+            </label>
+        </div>
     </div>
 </div>
