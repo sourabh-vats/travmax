dc56bcb502e6bb60870f5057013e98f7c4207248
sourabhvatscoder <sourabhvats96@gmail.com>
2023-04-18 10:11:01 +0530

    dev

--- vc_application/views/admin/start.php
+++ vc_application/views/admin/start.php
@@ -5,12 +5,14 @@
         Start your <br>
         travel journey now
     </p>
+    <br>
     <p class="text_1">
         Please select <br>
         a tour package <br>
         & <br>
         a payment plan to continue.
     </p>
+    <br>
     <a href="/admin/select_package" class="primary_btn">Continue</a>
 </div>
 </div>
