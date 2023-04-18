9c19c1af3df08c52babfaf1be25ed06d3a487122
sourabhvatscoder <sourabhvats96@gmail.com>
2023-04-18 09:50:56 +0530

    dev

--- vc_application/views/admin/start.php
+++ vc_application/views/admin/start.php
@@ -1,3 +1,14 @@
 <div class="row" id="wrapper">
-
+<div id="content_box">
+    <p class="text_1">
+        Hi Manoj, <br>
+        Start Your Travel Journey Now
+    </p>
+    <p class="text_1">
+        Please select a tour package <br>
+        & <br>
+        a payment plan to continue.
+    </p>
+    <a href="admin/select_package" class="primary_btn">Continue</a>
+</div>
 </div>
