6be66c95ead6f47b83b801b40be51ba2d59b4d44
sourabhvatscoder <sourabhvats96@gmail.com>
2023-04-22 10:42:25 +0530

    dev

--- vc_application/views/admin/select_plan.php
+++ vc_application/views/admin/select_plan.php
@@ -1,5 +1,5 @@
 <div class="row" id="pick_a_plan_section">
-    <h1 class="text-center my-5">You have selected <span id="pick_a_plan_selected_package_name"><?php echo $package_data[0]["name"];?></span> package. Please select a payment plan.</h1>
+    <h1 class="text-center my-5">You have selected <span id="pick_a_plan_selected_package_name"><?php echo $package_data[0]["name"]; ?></span> package. Please select a payment plan.</h1>
     <div class="card-group text-center">
         <div class="card plan_box" id="travnow_plan">
             <div class="card-body">
@@ -41,14 +41,14 @@
             </div>
         </div>
     </div>
+    <a href="/admin/confirm_plan?package=<?php echo $_GET["package"]; ?>&plan=" id="confirm_btn" class="primary_btn">Continue</a>
     <?php
     $user = $profile[0];
     $attributes = array('class' => 'form');
     echo form_open_multipart(base_url() . 'admin/select_package', $attributes);
     ?>
-    <input type="hidden" name="package_id" value="<?php echo $package_data[0]["id"];?>">
+    <input type="hidden" name="package_id" value="<?php echo $package_data[0]["id"]; ?>">
     <input type="hidden" name="payment_type">
     <!-- <button class="btn btn-lg btn-primary my-3" id="book_package_btn" type="submit" disabled>Book</button> -->
     <?php echo form_close(); ?>
-    <a href="/admin/confirm_plan?package=<?php echo $_GET["package"]; ?>&plan=" id="confirm_btn" class="primary_btn">Continue</a>
 </div>
