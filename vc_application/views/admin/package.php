c2423897b1eaa6e6390b6ad47ab5de306a157baf
sourabhvatscoder <sourabhvats96@gmail.com>
2023-04-22 10:26:47 +0530

    dev

--- vc_application/views/admin/package.php
+++ vc_application/views/admin/package.php
@@ -24,6 +24,6 @@ $package = $package_data[0];
         Inclusions: Ex Delhi <br>
         Flight, Hotels, Transfers, Breakfast, Sightseeing
     </p>
-    <p>Terms & Conditions</p>
+    <a id="package_terms" href="/terms_of_use" class="text-center">Terms & Conditions</as>
     <a href="/admin/select_plan?package=<?php echo $_GET["package"]; ?>" class="primary_btn">Continue</a>
 </div>
