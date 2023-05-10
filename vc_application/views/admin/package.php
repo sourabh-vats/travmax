ed378b1d28000b9df7a49e034ba9bd5d3771fd09
sourabhvatscoder <sourabhvats96@gmail.com>
2023-05-10 14:08:40 +0530

    dev

--- vc_application/views/admin/package.php
+++ vc_application/views/admin/package.php
@@ -1,6 +1,8 @@
 <?php
 $package = $package_data[0];
 $amount_with_tax = $package["total"] + ($package["total"] * 0.5) + ($package["total"] * 0.5);
+echo $package["total"] * 0.5;
+die();
 ?>
 <div class="row" id="wrapper">
     <div id="content_box">
